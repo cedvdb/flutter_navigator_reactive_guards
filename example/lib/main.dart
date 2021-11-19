1aca96409b4cf347279648e0781a9ab63d582544
cedvdb <cedvandenbosch@gmail.com>
2021-11-19 21:08:13 +0100

    example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,49 @@
+import 'package:beamer/beamer.dart';
+import 'package:example/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:navigator_reactive_guards/navigator_reactive_guards.dart';
+import 'package:rxdart/rxdart.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+// reactive guards
+
+class AuthGuard extends ReactiveGuard<bool> {
+  final BehaviorSubject<bool> _authStatusController =
+      BehaviorSubject.seeded(false);
+
+  login() {
+    _authStatusController.add(true);
+  }
+
+  logout() {
+    _authStatusController.add(false);
+  }
+
+  @override
+  ReactiveGuardResult resolve(bool streamValue, String currentRoute) {
+    if (streamValue == true) {
+      if (currentRoute == Routes.login) {
+        return const Redirect(target: Routes.home);
+      } else {
+        return const Next();
+      }
+    } else {
+      if (currentRoute == Routes.login) {
+        return const Next();
+      } else {
+        return const Redirect(target: Routes.login);
+      }
+    }
+  }
+
+  @override
+  Stream<bool> get stream => _authStatusController.stream;
+}
+
+// app
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -10,106 +51,64 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    final authGuard = AuthGuard();
+
+    /// beamer used here but other libraries can be used
+    final routerDelegate = BeamerDelegate(
+      locationBuilder: RoutesLocationBuilder(
+        routes: {
+          Routes.login: (context, state) => LoginPage(
+                onLoginPress: () => authGuard.login(),
+              ),
+          Routes.home: (context, state) => HomePage(
+                onLogoutPress: () => authGuard.logout(),
+              ),
+        },
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+    return BeamerProvider(
+      routerDelegate: routerDelegate,
+      child: MaterialApp.router(
+        routeInformationParser: BeamerParser(),
+        routerDelegate: routerDelegate,
+        builder: (ctx, child) => ReactiveGuardDispatcher(
+          child: child ?? Container(),
+          guards: [authGuard],
+          onRedirectRequest: (req) => Beamer.of(ctx).beamToNamed(req.target),
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class LoginPage extends StatelessWidget {
+  final Function() onLoginPress;
+  const LoginPage({
+    Key? key,
+    required this.onLoginPress,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return ElevatedButton(
+      onPressed: () {},
+      child: const Text('login'),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final Function() onLogoutPress;
+  const HomePage({
+    Key? key,
+    required this.onLogoutPress,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: const Text('logout'),
     );
   }
 }
---
+++ example/lib/routes.dart
@@ -0,0 +1,4 @@
+class Routes {
+  static const login = '/login';
+  static const home = '/home';
+}
